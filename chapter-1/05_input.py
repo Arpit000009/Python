dfc36babd07bd705f82d6be4263ab947f7b68170
Arpit000009 <100606591+Arpit000009@users.noreply.github.com>
2025-07-28 02:56:31 +0530

    first

---
+++ chapter-1/05_input.py
@@ -0,0 +1,6 @@
+# input("") = Afunction that prompts the user to entar data
+#       Returns the entered data as string
+
+name = input("what is your name?:")
+
+print(f"hello {name}!")
--- chapter-1/first.py
+++ chapter-1/first.py
@@ -1,2 +1,5 @@
-import flask
-print("hello world")
+length = float(input("enter the length: "))
+width = float(input("enter the width: "))
+area = length * width
+
+print(f"the area of rectangle is: {area}cm")
