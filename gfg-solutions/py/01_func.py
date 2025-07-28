1ec2d7bb76e5856bd6a74c3bc6da09d5f29e3b51
Arpit000009 <100606591+Arpit000009@users.noreply.github.com>
2025-07-28 19:34:35 +0530

    functions

---
+++ gfg-solutions/py/01_func.py
@@ -0,0 +1,14 @@
+""" Here one integer n is given. You need to write the complete function returnValueFunction that takes n as a parameter and uses the return keyword to return double the value of n.
+
+Examples:
+
+Input: n = 2
+Output: 4
+Explanation: 2 * 2 = 4"""
+
+#code here
+def returnValueFunction(n):
+    return n*2
+
+n = 2
+print(returnValueFunction(n))
---
+++ gfg-solutions/py/02_no_argu.py
@@ -0,0 +1,11 @@
+#User function Template for python3
+
+#Write the helloFunction below. It should have one statement
+#print("Hello")
+#code here
+
+def helloFunction():
+    print("Hello")
+
+
+helloFunction()
