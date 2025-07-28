82313708792c6426e59d8895fc3840f45bf56bf6
Arpit000009 <100606591+Arpit000009@users.noreply.github.com>
2025-07-28 23:25:47 +0530

    prog

---
+++ gfg-solutions/py/05_jump_squares.py
@@ -0,0 +1,21 @@
+"""Given a positive integer x, the task is to print the numbers from 1 to x in the order as 12, 22, 32, 42, 52, ... (in increasing order).
+
+Example:
+
+Input: x = 10
+Output: 1 4 9
+Explanation:From 1 to 10, numbers in powers of 2 are, 12, 22, 32 as 1, 4 and 9.
+Constraints:
+2 <= x <= 103"""
+
+def printIncreasingPower(x):
+    #code here
+    # Loop to jump in powers of 2
+    y = 1
+    while(x>=y*y):
+        #code here
+        if(y*y<=x):
+            print ( y*y, end = " ")
+            y+=1
+        
+        #code here
